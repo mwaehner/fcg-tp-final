a8f7cbc629db87ab881dd182257341fcdf0763c6
Alejandro Cortez <alejandro17198@gmail.com>
2021-11-09 20:32:13 -0300

    Hice cositas jeje

--- boxdrawer.js
+++ boxdrawer.js
@@ -6,12 +6,15 @@ class BoxDrawer
 		// 1. Compilamos el programa de shaders
 		this.prog = InitShaderProgram( boxVS, boxFS );
 		
+		
+		
 		// 2. Obtenemos los IDs de las variables uniformes en los shaders
 		this.mvp = gl.getUniformLocation( this.prog, 'mvp' );
 		this.mv = gl.getUniformLocation( this.prog, 'mv' );
 		this.mn = gl.getUniformLocation( this.prog, 'mn' );
 		this.light = gl.getUniformLocation( this.prog, 'light' );
 		this.alpha = gl.getUniformLocation( this.prog, 'alpha' );
+		this.waterLevel = gl.getUniformLocation( this.prog, 'waterLevel' );
 		
 		// 3. Obtenemos los IDs de los atributos de los vértices en los shaders
 		this.vertPos = gl.getAttribLocation( this.prog, 'pos' );
@@ -34,6 +37,9 @@ class BoxDrawer
 
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.normBuffer);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+		gl.useProgram(this.prog );
+		var waterLevel = (Math.random() * 0.2) + 0.9;
+		gl.uniform1f( this.waterLevel, waterLevel);
 
 	}
 	
@@ -82,11 +88,13 @@ class BoxDrawer
 
 // Vertex shader 
 var boxVS = `
+	precision mediump float;
 	attribute vec3 pos;
 	attribute vec3 normpos;
 
 	uniform mat4 mvp;
 	uniform mat4 mv;
+	uniform float waterLevel;
 
 	varying float distance;
 	varying vec3 normCoord;
@@ -94,10 +102,9 @@ var boxVS = `
 
 	void main()
 	{
-		
 		distance = sqrt(pos.x*pos.x + pos.y*pos.y + pos.z*pos.z);
-		if(distance<1.0){
-			gl_Position = mvp * vec4(normalize(pos),1);
+		if(distance<waterLevel){
+			gl_Position = mvp * (vec4(normalize(pos)*waterLevel,1));
 		} else {
 			gl_Position = mvp * vec4(pos,1);
 		}
@@ -114,6 +121,7 @@ var boxFS = `
 	uniform mat3 mn;
 	uniform vec3 light;
 	uniform float alpha;
+	uniform float waterLevel;
 
 	varying float distance;
 	varying vec3 normCoord;
@@ -123,14 +131,21 @@ var boxFS = `
 	{
 		vec4 I = vec4(1.0, 1.0, 1.0, 1.0);
 		vec4 Kd;
-		if(distance<0.99){
+		float snowLevel = 1.15;
+		if(distance<waterLevel){
 			// agua
 			Kd = vec4(vec3(0,0,1),1);
-		} else {
+		} else if(distance>snowLevel) {
+			// nieve
+			Kd = vec4(vec3(1.0,1.0,1.0)*distance,1);
+		} else if(distance>=waterLevel && distance < (waterLevel + 0.01)) {
+			// arena
+			Kd = vec4(vec3(0.76,0.69,0.50)*distance,1);
+		}
+		else {
 			// tierra
 			Kd = vec4(vec3(0.2,0.5,0.3)*distance,1);
 		}
-		// TODO: agregar nieve
 		vec4 Ks = vec4(1.0, 1.0, 1.0, 1.0);
 
 		// TODO (opcional): refraccion solo para nieve y agua
@@ -142,9 +157,15 @@ var boxFS = `
 		float cos_theta = dot(n, l);
 		float cos_sigma = dot(vertCoord, vec4(r, 1) );
 		float cos_omega = dot(n, h);
+		
+		vec4 refractionM;
+		if (distance < waterLevel || distance > snowLevel) {
+			refractionM = Ks;
+		} else {
+			refractionM = vec4(0.0, 0.0, 0.0, 0.0);
+		}
 	
-		gl_FragColor = I * max(0.0, cos_theta) * (Kd + Ks * pow(max(0.0, cos_omega), alpha) / cos_theta);
-		//gl_FragColor = vec4(1,1,1,1);
+		gl_FragColor = I * max(0.0, cos_theta) * (Kd + refractionM * pow(max(0.0, cos_omega), alpha) / cos_theta);
 		
 	}
 `;
